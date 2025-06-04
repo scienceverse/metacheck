6b468fd811a5f56bbbe50331f84a9fef80e66cf4
Lisa DeBruine <debruine@gmail.com>
2025-06-04 17:02:12 +0100

    add load text

--- R/zzz.R
+++ R/zzz.R
@@ -13,5 +13,25 @@
   invisible()
 }
 
+.onAttach <- function(libname, pkgname) {
+  stripe <- paste0(
+    "\033[31m***\033[39m",
+    "\033[33m***\033[39m",
+    "\033[32m***\033[39m",
+    "\033[34m***\033[39m",
+    "\033[35m***\033[39m"
+  )
+  paste(
+    "\n",
+    stripe,
+    "Welcome to PaperCheck. For support and examples visit:",
+    "https://scienceverse.github.io/papercheck/",
+    "\\26A0 This software is an alpha version, so please check any results. ",
+    "\\26A0 False positives and false negatives will occur at unknown rates.",
+    stripe,
+    sep = "\n"
+  ) %>% packageStartupMessage()
+}
+
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
