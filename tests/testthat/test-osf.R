fa35b37f94f742cec6bf35c4d4f3a5f27c727a06
Lisa DeBruine <debruine@gmail.com>
2025-06-12 23:53:56 +0100

    OSF function updates - so many!

--- tests/testthat/test-osf.R
+++ tests/testthat/test-osf.R
@@ -1,9 +1,15 @@
+verbose(FALSE)
+# options(papercheck.osf.api = "https://api.osf.io/v2/")
+# osf_delay(0)
+
 test_that("exists", {
+  expect_true(is.function(papercheck::osf_check_id))
+
   expect_true(is.function(papercheck::osf_retrieve))
   expect_no_error(helplist <- help(osf_retrieve, papercheck))
 
-  expect_true(is.function(papercheck::osf_contents))
-  expect_no_error(helplist <- help(osf_contents, papercheck))
+  expect_true(is.function(papercheck::osf_info))
+  expect_no_error(helplist <- help(osf_info, papercheck))
 
   expect_true(is.function(papercheck::osf_delay))
   expect_no_error(helplist <- help(osf_delay, papercheck))
@@ -13,143 +19,256 @@ test_that("exists", {
 
 })
 
-test_that("errors", {
-  skip_on_cran()
-  skip_on_covr()
-  skip_if_offline("osf.io")
-
-  # invalid ID
-  id <- "noid"
-  expect_warning(invalid <- osf_retrieve(id))
-  expect_equal(invalid$id, id)
-  expect_equal(invalid$osf_type, "invalid")
-
-  # valid but unfound ID
-  id <- "noid2"
-  expect_warning(unfound <- osf_retrieve(id))
-  expect_equal(unfound$id, id)
-  expect_equal(unfound$osf_type, "unfound")
+test_that("osf_api_check", {
+  status <- osf_api_check()
+  possible <- c("ok", "too many requests",
+                "server error", "unknown")
+  expect_true(status %in% possible)
 })
 
-test_that("osf_retrieve", {
-  skip_on_cran()
-  skip_on_covr()
-  skip_if_offline("api.osf.io")
+test_that("osf_headers", {
+  header <- osf_headers()
+  expect_equal(header$`User-Agent`, "Papercheck")
+})
 
-  # user
-  id <- "4i578"
-  user <- osf_retrieve(id)
-  expect_equal(user$name, "Lisa DeBruine")
-  expect_equal(user$id, id)
-  expect_equal(user$orcid, "0000-0002-7523-5539")
-  expect_equal(user$osf_type, "user")
+test_that("osf_check_id", {
+  # 5-letter
+  osf_id <- "pngda"
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, osf_id)
 
+  # vector
+  osf_id <- c("pngda", "8c3kb")
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, osf_id)
+
+  # vector with invalid values
+  osf_id <- c("pngda", "xxx", "8c3kb")
+  expect_warning(checked_id <- osf_check_id(osf_id))
+  expect_equal(checked_id, c("pngda", NA, "8c3kb"))
+
+  # waterbutler id
+  osf_id <- "6846ed88e49694cd45ab8375"
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, osf_id)
+
+  # invalidwaterbutler id
+  osf_id <- "6846ed894cd45ab8375"
+  expect_warning(checked_id <- osf_check_id(osf_id))
+  expect_true(is.na(checked_id))
+
+  # urls
+  osf_id <- "https://osf.io/pngda"
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, "pngda")
+
+  osf_id <- "http://osf.io/pngda"
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, "pngda")
+
+  # url with no http
+  osf_id <- "osf.io/pngda"
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, "pngda")
+
+  # deal with rogue whitespace
+  osf_id <- "osf .io/pngda"
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, "pngda")
+
+  osf_id <- "xx"
+  expect_warning(checked_id <- osf_check_id(osf_id))
+  expect_true(is.na(checked_id))
+
+  # view-only link
+  osf_id <- "https://osf.io/pngda/?view_only=5acf039f24ac4ea28afec473548dd7f4"
+  checked_id <- osf_check_id(osf_id)
+  expect_equal(checked_id, "pngda")
+})
+
+test_that("osf_get_all_pages", {
+  osf_api <- getOption("papercheck.osf.api")
+
+  # fewer than 10
+  url <- sprintf("%s/nodes/pngda/files/osfstorage/", osf_api)
+  data <- osf_get_all_pages(url)
+  expect_equal(nrow(data), 2)
+
+  # more than 10
+  url <- sprintf("%s/nodes/yt32c/files/osfstorage/", osf_api)
+  data <- osf_get_all_pages(url)
+  expect_equal(nrow(data), 14)
+
+  # no results
+  url <- sprintf("%s/nodes/y6a34/files/osfstorage/", osf_api)
+  data <- osf_get_all_pages(url)
+  expect_equal(data, list())
+})
+
+test_that("osf_files", {
+  osf_id <- "pngda"
+  data <- osf_files(osf_id)
+  expect_equal(nrow(data), 2)
+
+  osf_id <- "yt32c"
+  data <- osf_files(osf_id)
+  expect_equal(nrow(data), 14)
+  expect_equal(data$filetype, rep("data", 14))
+
+  osf_id <- "y6a34"
+  data <- osf_files(osf_id)
+  expect_equal(nrow(data), 0)
+})
+
+test_that("osf_children", {
+  osf_id <- "pngda"
+  data <- osf_children(osf_id)
+  expect_equal(nrow(data), 2)
+
+  osf_id <- "y6a34"
+  data <- osf_children(osf_id)
+  expect_equal(nrow(data), 0)
+})
+
+test_that("osf_info", {
   # project
-  id <- "pngda"
-  project <- osf_retrieve(id)
-  expect_equal(project$name, "Papercheck Test")
-  expect_equal(project$id, id)
-  expect_equal(project$osf_type, "node")
-  expect_equal(project$category, "project")
-  expect_true(is.na(project$parent))
-  expect_equal(project$project, id)
+  osf_id <- "pngda"
+  info <- osf_info(osf_id)
+  expect_equal(info$osf_id, osf_id)
+  expect_equal(info$osf_type, "nodes")
+  expect_equal(info$name, "Papercheck Test")
 
   # component
-  id <- "6nt4v"
-  comp <- osf_retrieve(id)
-  expect_equal(comp$name, "Processed Data")
-  expect_equal(comp$id, id)
-  expect_equal(comp$osf_type, "node")
-  expect_equal(comp$category, "data")
-  expect_equal(comp$parent, "ckjef")
-  expect_equal(comp$project, project$id)
+  osf_id <- "6nt4v"
+  info <- osf_info(osf_id)
+  expect_equal(info$osf_id, osf_id)
+  expect_equal(info$osf_type, "nodes")
+  expect_equal(info$name, "Processed Data")
 
-  # file ----
-  id <- "75qgk"
-  file <- osf_retrieve(id)
-  expect_equal(file$name, "processed-data.csv")
-  expect_equal(file$id, id)
-  expect_equal(file$osf_type, "file")
-  #expect_true(file$downloads > 0)
-  expect_equal(file$parent, comp$id)
-  expect_equal(file$project, project$id)
+  # file
+  osf_id <- "75qgk"
+  info <- osf_info(osf_id)
+  expect_equal(info$osf_id, osf_id)
+  expect_equal(info$osf_type, "files")
+  expect_equal(info$kind, "file")
+  expect_equal(info$name, "processed-data.csv")
 
-  # file parent project
-  id <- "75qgk"
-  parent_proj <- osf_parent_project(id)
-  expect_equal(parent_proj, project$id)
+  # preprint
+  osf_id <- "xp5cy"
+  info <- osf_info(osf_id)
+  expect_true(grepl(osf_id, info$osf_id))
+  expect_equal(info$osf_type, "preprints")
+  expect_equal(info$name, "Understanding mixed effects models through data simulation")
 
-  # registration
-  id <- "8c3kb"
-  reg <- osf_retrieve(id)
-  expect_equal(reg$name, "Understanding mixed effects models through data simulation")
-  expect_equal(reg$id, id)
-  expect_equal(reg$osf_type, "registration")
-  expect_equal(reg$parent, "3cz2e")
-  expect_equal(reg$project, "3cz2e")
+  # user
+  # osf_id <- "4i578"
+  # info <- osf_info(osf_id)
+  # expect_equal(info$osf_id, osf_id)
+  # expect_equal(info$osf_type, "users")
+  # expect_equal(info$name, "Lisa DeBruine")
 
-  # duplicate IDs
-  id <- c("4i578", "4i578")
-  expect_message(user <- osf_retrieve(id), "Retrieving info from 4i578...", fixed = TRUE)
-  expect_equal(user$name, rep("Lisa DeBruine", 2))
-  expect_equal(user$id, id)
-  expect_equal(user$orcid, rep("0000-0002-7523-5539", 2))
-  expect_equal(user$osf_type, rep("user", 2))
+  # reg
+  osf_id <- "8c3kb"
+  info <- osf_info(osf_id)
+  expect_equal(info$osf_id, osf_id)
+  expect_equal(info$osf_type, "registrations")
+  expect_equal(info$name, "Understanding mixed effects models through data simulation")
 
-  # URL
-  id <- "osf.io/pngda"
-  project <- osf_retrieve(id)
-  expect_equal(project$name, "Papercheck Test")
-  expect_equal(project$id, id)
-  expect_equal(project$osf_id, "pngda")
-  expect_equal(project$osf_type, "node")
-  expect_equal(project$category, "project")
-  expect_true(is.na(project$parent))
-  expect_equal(project$project, "pngda")
+  # private
+  osf_id <- "ybm3c"
+  info <- osf_info(osf_id)
+  expect_equal(info$osf_id, osf_id)
+  expect_equal(info$osf_type, "private")
+  expect_equal(info$public, FALSE)
 
-  # https://URL
-  id <- "https://osf.io/pngda"
-  project <- osf_retrieve(id)
-  expect_equal(project$name, "Papercheck Test")
-  expect_equal(project$id, id)
-  expect_equal(project$osf_id, "pngda")
-  expect_equal(project$osf_type, "node")
-  expect_equal(project$category, "project")
-  expect_true(is.na(project$parent))
-  expect_equal(project$project, "pngda")
+  # invalid
+  osf_id <- "xx"
+  expect_warning(info <- osf_info(osf_id))
+  expect_equal(info$osf_id, osf_id)
+  expect_equal(info$osf_type, "invalid")
 
-  # list
-  id <- c(user = "4i578", project = "pngda", component = "6nt4v", file = "75qgk", reg = "8c3kb")
-  osf_list <- osf_retrieve(id)
-  expect_equal(osf_list$id, unname(id))
-  expect_equal(osf_list$osf_type, c("user", "node", "node", "file", "registration"))
+  # valid but not found
+  osf_id <- "xxxxx"
+  expect_warning(info <- osf_info(osf_id))
+  expect_equal(info$osf_id, osf_id)
+  expect_equal(info$osf_type, "unfound")
 })
 
-test_that("osf_contents", {
-  #skip("Long process")
-  skip_on_cran()
-  skip_on_covr()
-  skip_if_offline("api.osf.io")
 
-  id <- "pngda"
-  contents <- osf_contents(id)
-  expect_true(all(c("ckjef", "j3gcx", "6nt4v") %in% contents$osf_id))
+test_that("osf_retrieve", {
+  examples <- c(project = "pngda",
+                component = "https://osf.io/6nt4v",
+                private = "ybm3c",
+                file = "osf.io/75qgk",
+                preprint = "xp5cy",
+                #user = "4i578",
+                reg = "8c3kb",
+                duplicate = "6nt4v",
+                bad = "xx")
+  osf_url <- data.frame(
+    url = examples,
+    type = names(examples)
+  )
+  expect_warning(table <- osf_retrieve(osf_url))
+  expect_equal(table$url, osf_url$url)
+  expect_equal(table$type, osf_url$type)
+  expect_equal(table[2, 3:10], table[7, 3:10], ignore_attr = TRUE)
 
-  # duplicate links
-  id <- c("pngda", "pngda")
-  contents2 <- osf_contents(id)
-  expect_equal(contents, contents2)
+  # vector
+  osf_url <- "pngda"
+  table <- osf_retrieve(osf_url)
+  expect_equal(table$osf_url, osf_url)
+  expect_equal(table$name, "Papercheck Test")
 
-  # multiple ids
-  id <- c("j3gcx", "6nt4v")
-  contents <- osf_contents(id)
-  expect_true(all(c("6846ed88e49694cd45ab8375", "6846ed6a29684b023953943e") %in% contents$osf_id))
-  expect_true(all(id %in% contents$parent))
+  # table with id_col
+  osf_url <- data.frame(
+    id = 100,
+    osf_id = "pngda"
+  )
+  id_col <- "osf_id"
+  table <- osf_retrieve(osf_url, id_col)
+  expect_equal(table$osf_id, osf_url$osf_id)
+  expect_equal(table$name, "Papercheck Test")
 
-  # > 10 contents
-  id <- "yt32c"
-  contents <- osf_contents(id)
-  expect_equal(contents$type, "missing")
+  # recursive
+  osf_url <- "yt32c"
+  table <- osf_retrieve(osf_url, recursive = TRUE)
+  expect_equal(nrow(table), 15)
+  expect_equal(table$parent, rep(c("ckjef", "yt32c"), c(1, 14)))
+
+  # recursive with duplicates and NA vector
+  osf_url <- c("yt32c", "yt32c", NA)
+  table <- osf_retrieve(osf_url, recursive = TRUE)
+  expect_equal(nrow(table), 1 + 14)
+
+  # recursive with duplicates and NA table
+  osf_url <- data.frame(parent_id = c("yt32c", "yt32c", NA),
+                        n = 1:3)
+  expect_warning(table <- osf_retrieve(osf_url, recursive = TRUE))
+  expect_equal(nrow(table), 3 + 14)
+  expect_equal(table$n, c(1:3, rep(NA, 14)))
+})
+
+test_that("osf_parent_project", {
+  # has parent project
+  osf_id <- "yt32c"
+  parent <- osf_parent_project(osf_id)
+  expect_equal(parent, "pngda")
+
+  # is a parent project
+  osf_id <- "pngda"
+  parent <- osf_parent_project(osf_id)
+  expect_equal(parent, "pngda")
+
+  # preprint
+  osf_id <- "xp5cy"
+  parent <- osf_parent_project(osf_id)
+  expect_equal(parent, "3cz2e")
+
+  # invalid ID
+  osf_id <- "pda"
+  expect_warning(parent <- osf_parent_project(osf_id))
+  expect_true(is.na(parent))
 })
 
 test_that("summarize_contents", {
@@ -157,11 +276,30 @@ test_that("summarize_contents", {
   skip_on_covr()
   skip_if_offline("api.osf.io")
 
-  id <- "pngda"
-  contents <- osf_contents(id)
+  osf_id <- "pngda"
+  contents <- osf_retrieve(osf_id, recursive = TRUE)
 
   summary <- summarize_contents(contents)
+
+  readme <- dplyr::filter(summary, name == "README")
+  expect_equal(readme$best_guess, "readme")
+  expect_equal(readme$is_data, FALSE)
+  expect_equal(readme$is_code, FALSE)
+  expect_equal(readme$is_codebook, FALSE)
+  expect_equal(readme$is_readme, TRUE)
+
+  # handle zero results and/or OSF down
+  summary <- summarize_contents(data.frame())
+  expect_equal(nrow(summary), 0)
 })
 
-osf_links <- module_run(psychsci, "osf_check")
-osf_info <- osf_retrieve(osf_links$table$text)
+
+test_that("rate limiting", {
+  skip("long")
+
+  osf_id <- "pngda"
+
+  for (i in 1:110) {
+    info <- osf_info(osf_id)
+  }
+})
