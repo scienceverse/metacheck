a3d7ac9023c1e19d52f4a0d6836113eb764db77d
Lisa DeBruine <debruine@gmail.com>
2025-06-25 11:32:05 +0100

    add p-values and comparator to all_p_values module

--- inst/modules/all_p_values.R
+++ inst/modules/all_p_values.R
@@ -31,18 +31,29 @@ all_p_values <- function(paper, ...) {
   )
 
   # detailed table of results ----
-  table <- search_text(paper, pattern, return = "match",
+  p <- search_text(paper, pattern, return = "match",
                        perl = TRUE, ignore.case = FALSE)
 
+  # get operator
+  pattern <- paste0("[", operators, "]{1,2}")
+  matches <- gregexpr(pattern, p$text, perl = TRUE)
+  p$p_comp <- regmatches(p$text, matches) |> sapply(`[[`, 1)
+
+  # get value
+  pattern <- paste0("(?<=[", operators, "]{1,2}).*$")
+  matches <- gregexpr(pattern, p$text, perl = TRUE)
+  p$p_value <- regmatches(p$text, matches) |> trimws()
+  p$p_value <- suppressWarnings(as.numeric(p$p_value))
+
   # summary output for paperlists ----
-  summary_table <- dplyr::count(table, id, name = "p_values")
+  summary_table <- dplyr::count(p, id, name = "p_values")
 
   # determine the traffic light ----
-  tl <- if (nrow(table)) "info" else "na"
+  tl <- if (nrow(p)) "info" else "na"
 
   # return a list ----
   list(
-    table = table,
+    table = p,
     summary = summary_table,
     na_replace = 0,
     traffic_light = tl
