4e1ec315a4cc29286a419fc1456393e8e21e00b9
Jakub Werner <git@jakubwerner.com>
2025-10-07 21:57:41 +0200

    deprecated pdf2grobid support

--- inst/plumber/api.R
+++ inst/plumber/api.R
@@ -6,15 +6,13 @@ library(plumber)
 library(papercheck)
 
 #* @apiTitle Papercheck API
-#* @apiDescription API for analyzing academic papers. Upload PDFs or GROBID XML files to extract metadata, authors, references, and more.
+#* @apiDescription API for analyzing academic papers. Upload GROBID TEI XML to extract metadata, authors, references, and more.
 
 #* @plumber
 function(pr) {
   pr %>%
-    # Paper analysis endpoints - upload PDF/XML to analyze
-    pr_mount("/paper", plumber::pr("endpoints/paper.R")) %>%
-    # GROBID processing endpoints - convert PDF to XML
-    pr_mount("/grobid", plumber::pr("endpoints/grobid.R"))
+    # Paper analysis endpoints - upload XML to analyze
+    pr_mount("/paper", plumber::pr("endpoints/paper.R"))
 }
 
 #* Health check endpoint
