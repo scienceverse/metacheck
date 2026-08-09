880ca09557ac01c8aa2a8f730cdb30fe5642305d
Lakens <D.Lakens@tue.nl>
2026-08-09 07:20:49 +0200

    delete test

--- tests/testthat/.metacheck_repo_cache/researchbox.org_4377/unzipped/Code/Study 1.r
+++
@@ -1,9 +0,0 @@
-# this is a test script
-
-library(papercheck)
-
-a <- 1 + 1
-
-abs_path_mac <- read.csv("/lisa/file.csv")
-abs_path_pc <- read.csv("C:/lisa/file.csv")
-relative_path <- read.csv("file.csv")
